ec607b774aaa7bde78f4e168fd6dfba73515447b
thiagohendler1 <thiagohendler1@gmail.com>
2023-05-14 21:15:00 -0300

    Clase8

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
+from django.template import loader
 
 class Persona(object):
 
@@ -20,16 +21,22 @@ def saludo(request):   ##primera vista
 
     ahora = datetime.datetime.now()
 
+    '''
+    Antes de usar los cargadores:
+
+    #doc_externo = open("C:/Users/thiag/OneDrive/Escritorio/Thiago/PROGRAMACIÓN/cursodjango/Proyecto1/Proyecto1/plantillas/miplantilla.html")
     
-    doc_externo = open("C:/Users/thiag/OneDrive/Escritorio/Thiago/PROGRAMACIÓN/cursodjango/Proyecto1/Proyecto1/plantillas/miplantilla.html")
+    #plt = Template(doc_externo.read())
     
-    plt = Template(doc_externo.read())
+    #doc_externo.close()
+    '''
+    #Con los cargadores::
     
-    doc_externo.close()
+    doc_externo = loader.get_template('miplantilla.html')
     
-    ctx = Context({'nombre_persona':p1.nombre, 'apellido_persona': p1.apellido, 'fecha_ahora': ahora, 'temas': temas_curso})
+    #ctx = Context({'nombre_persona':p1.nombre, 'apellido_persona': p1.apellido, 'fecha_ahora': ahora, 'temas': temas_curso})
     
-    documento = plt.render(ctx)
+    documento = doc_externo.render({'nombre_persona':p1.nombre, 'apellido_persona': p1.apellido, 'fecha_ahora': ahora, 'temas': temas_curso})
     
     return HttpResponse(documento)
 
