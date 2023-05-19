e36d1ba646c2fea1ec2d83a948cec11c05d81f0c
thiagohendler1 <thiagohendler1@gmail.com>
2023-05-18 21:29:14 -0300

    Clase 19

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -8,6 +8,15 @@ class ClientesAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'direccion', 'tfno')
     search_fields = ('nombre', 'tfno')
 
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter = ('seccion',)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'fecha')
+    list_filter = ('fecha',)
+    date_hierarchy = 'fecha'
+
 admin.site.register(Clientes, ClientesAdmin)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
