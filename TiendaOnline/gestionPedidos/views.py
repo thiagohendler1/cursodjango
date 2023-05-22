93ad9e539d973bf41bc105dc0879f99a942a940d
thiagohendler1 <thiagohendler1@gmail.com>
2023-05-22 19:04:15 -0300

    Clase22

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 
 # Create your views here.
 
@@ -8,7 +9,22 @@ def busqueda_productos(request):
     return render (request, 'busqueda_productos.html')
 
 def buscar(request):
+    
+    if request.GET["prd"]:
 
-    mensaje = 'Articulo buscado: %r' %request.GET['prd']
+        #mensaje = 'Articulo buscado: %r' %request.GET['prd']
+        producto = request.GET["prd"]  
+        
+        articulos = Articulos.objects.filter(nombre__icontains=producto)
+                                #Funciona como la funcion LIKE en sql (el __icontains)
+                                #Busca en el campo nombre la palabra que esta almacenada en producto. 
+        #Una vez que ya tenemos el articulo que buscamos cargado, debemos
+        #crear un html para que nos muestre esta información en pantalla. 
+        
+        return render(request, 'resultados_busqueda.html', {'articulos': articulos, 'query': producto})
+        
+    else:
+        
+        mensaje = 'No has introducido nada.'
 
     return HttpResponse(mensaje) 
