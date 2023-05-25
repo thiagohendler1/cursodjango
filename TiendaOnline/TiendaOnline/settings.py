944b78ba344ea2fa9c98116cc42bcc4c5a2d5216
thiagohendler1 <thiagohendler1@gmail.com>
2023-05-25 13:01:33 -0300

    Clase24

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -122,3 +122,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+#        TLS es el protocolo que en este caso queremos utilizar
+#        Esta toda la info bien de esto en la pag oficial de Django.
+
+EMAIL_PORT = 587
+#Puerto que se va a usar para enviar estos correos electrónicos.
+
+EMAIL_HOST_USER=''
+#Usuario de nuestro correo
+
+EMAIL_HOST_PASSWORD=''
+#Contraseña de nuestro correo
--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionPedidos.models import Articulos
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -36,8 +38,20 @@ def buscar(request):
 
 def contacto(request):
     
-    if request.method == 'POST':
+    if request.method == 'POST':   #Si esto es True significa que hemos pulsado el botón de enviar. 
         
+        subject = request.POST['asunto']  #Nos traemos el asunto del mail
+        
+        message = request.POST['mensaje'] + ' ' + request.POST['email']  #Nos traemos el mensaje acompañado del mail por si deseamos contactar a esa persona nuevamente.
+        
+        email_from = settings.EMAIL_HOST_USER #Le decimos de donde tiene que venir ese gmail
+        
+        recipient_list = ['maildeprueba@prueba.com'] #Especificamos a donde queremos que viaje toda esa información. 
+                            #Aquí ponemos el mail a donde queremos que nos llegue toda la información. 
+
+        send_mail(subject, message, email_from, recipient_list) #Con esta función mandamos el mail
+        #Argiumentos: Asunto, mensaje, de donde viene el mail, hacia donde va dirigido. 
+
         return render(request, 'busqueda_productos.html')
     
     return render(request, 'contacto.html')
